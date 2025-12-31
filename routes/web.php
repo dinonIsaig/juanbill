d10c9a4a038396823f4431bd83dc6f60a7244c5a
Dinon Isaig <dinonbonagua@gmail.com>
2025-12-31 09:04:57 +0800

    feat(routes): added and modified some routes to utilized the controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\SignupController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\User\DashboardController;
+
 
 Route::get('/', function () {
     return view('account-type');
@@ -8,17 +12,16 @@ Route::get('/', function () {
 
 // User Routes
 Route::prefix('user')->group(function () {
-    Route::get('sign-up', function () {
-        return view('user.sign-up');
-    })->name('user.sign-up');
 
-    Route::get('dashboard', function () {
-        return view('user.dashboard');
-    })->name('user.dashboard');
+    Route::get('/sign-up', [SignupController::class, 'showRegister'])->name('user.sign-up');
 
-    Route::get('log-in', function () {
-        return view('user.log-in');
-    })->name('user.log-in');
+    Route::post('/sign-up', [SignupController::class, 'store'])->name('user.store');
+
+    Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+    Route::post('/login', [LoginController::class, 'login'])->name('user.login');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('user.log-out')->middleware('auth');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('user.dashboard')->middleware('auth');
 
     route::get('electricity', function () {
         return view('user.electricity');
@@ -28,6 +31,9 @@ Route::prefix('user')->group(function () {
         return view('user.water');
     })->name('user.water');
 
+    route::get('association', function () {
+        return view('user.association');
+    })->name('user.association');
 });
 
 // Admin Routes
