12b98857a6aa66f4cd9079096288a3b8afb1e4e1
Dinon Isaig <dinonbonagua@gmail.com>
2025-12-31 08:47:18 +0800

    Chore(Landing): added drop shadow for the card

--- resources/views/account-type.blade.php
+++ resources/views/account-type.blade.php
@@ -15,22 +15,22 @@
 
 @section('content')
 <div class="min-h-screen flex items-center justify-center p-4 relative overflow-hidden">
-    <div class="relative z-10 text-center max-w-md">
+    <div class="relative z-10 text-center max-w-xl max-md:w-md rounded-lg shadow-xl p-8">
 
         <!-- Logo -->
         <div class="flex justify-center mb-2">
-            <div class=" p-3 rounded-lg inline-block w-1/2">
+            <div class=" p-4 rounded-lg inline-block w-1/2">
                 <img src="{{ asset('build/assets/images/accountTypePage/Logo.png') }}" alt="Logo">
             </div>
         </div>
 
         <!-- Text -->
-        <h1 class="text-4xl font-bold text-text-primary mb-3">I'm signing in as...</h1>
-        <p class="text-neutral-gray text-lg mb-12">Select your account type</p>
+        <h1 class="text-xl font-bold text-text-primary max-sm:text-md">I'm signing in as...</h1>
+        <p class="text-neutral-gray text-lg mb-8 max-sm:text-sm">Select your account type</p>
 
         <!-- Action Buttons -->
-        <div class="space-y-4">
-            <a href="{{ route('user.sign-up') }}" class="block">
+        <div class="space-y-3">
+            <a href="{{ route('login') }}" class="block">
                 <button class="user-btn w-2/3 text-lg font-semibold">User</button>
             </a>
             <a href="{{ route('admin.log-in') }}" class="block">
