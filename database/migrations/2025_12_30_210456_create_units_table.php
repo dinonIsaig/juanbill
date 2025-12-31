d2c836346e4b9cba206c4bff5d0e6b6a892aa891
Dinon Isaig <dinonbonagua@gmail.com>
2025-12-31 08:55:27 +0800

    test: added schemas and seeders for the database

---
+++ database/migrations/2025_12_30_210456_create_units_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+
+            // Unit Details
+            $table->string('unit_area', 50)->nullable();
+            $table->integer('bldg_no');
+            $table->integer('floor_no');
+            $table->integer('unit_no');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('units');
+    }
+};
---
+++ database/seeders/UnitSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UnitSeeder extends Seeder
+{
+    public function run()
+    {
+        $bldgs = 3;
+        $floors = 3;
+        $units_per_floor = 10;
+
+        $units = [];
+        for ($bldg = 1; $bldg <= $bldgs; $bldg++) {
+            for ($floor = 1; $floor <= $floors; $floor++) {
+                for ($unit = 1; $unit <= $units_per_floor; $unit++) {
+
+                    $custom_id = intval($bldg . $floor . sprintf('%02d', $unit));
+
+                    $units[] = [
+                        'id' => $custom_id,
+                        'unit_area' => '45sqm',
+                        'bldg_no' => $bldg,
+                        'floor_no' => $floor,
+                        'unit_no' => $unit,
+                    ];
+                }
+            }
+        }
+        DB::table('units')->insert($units);
+    }
+}
