cd4ce7ab10fa18f103217a66876af206e6679421
Dinon Isaig <dinonbonagua@gmail.com>
2026-01-02 13:56:51 +0800

    feat: added js files for toggles password and notification flashin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 import { Chart, registerables } from 'chart.js';
 import './filter.js';
 import './multi-step-form.js';
+import './notification-flash.js';
+import './toggle-password.js'
+
 
 Chart.register(...registerables);
 
