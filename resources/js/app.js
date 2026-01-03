841860f547136caef33926ab4cec1b61b30923d6
Dinon Isaig <dinonbonagua@gmail.com>
2026-01-04 05:04:19 +0800

    buildt: added alpine js in the dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
 import './bootstrap';
 import { Chart, registerables } from 'chart.js';
+import Alpine from 'alpinejs';
 import './filter.js';
 import './multi-step-form.js';
 import './notification-flash.js';
-import './toggle-password.js'
-
+import './toggle-password.js';
 
 Chart.register(...registerables);
 
 window.Chart = Chart;
+
+window.Alpine = Alpine;
+
+Alpine.start();
